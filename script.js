de2a0290fd66266ec082501f1e580c1d71720a2c
trancongduu <126306009+trancongduu@users.noreply.github.com>
2025-03-29 19:14:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 console.log("File JavaScript đã được tải thành công!");
 alert("File JavaScript đã hoạt động!");
+
+let audio = document.getElementById("myAudio");
+  audio.setAttribute("autoplay", "");
+  audio.muted = true; // Tắt tiếng để autoplay hoạt động
+
+document.addEventListener("click", () => {
+    let audio = document.getElementById("myAudio");
+    audio.play();
+  }, { once: true }); // Chạy một lần khi người dùng nhấp
