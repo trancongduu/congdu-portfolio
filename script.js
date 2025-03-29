06126e76fdce1f2f344b9e62d3568ad1a37d780b
trancongduu <126306009+trancongduu@users.noreply.github.com>
2025-03-29 19:43:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 console.log("File JavaScript đã được tải thành công!");
 alert("File JavaScript đã hoạt động!");
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".tab-projects").addEventListener("click", function (event) {
-        event.preventDefault(); // Ngăn chặn hành vi mặc định của thẻ <a>
-        
-        const projectSection = document.querySelector("section#project");
-        if (projectSection) {
-            projectSection.scrollIntoView({ behavior: "smooth" });
-        }
-    });
-});
+let audio = document.getElementById("myAudio");
+  audio.setAttribute("autoplay", "");
+  audio.muted = true; // Tắt tiếng để autoplay hoạt động
+document.addEventListener("click", () => {
+    let audio = document.getElementById("myAudio");
+    audio.play();
+  }, { once: true }); // Chạy một lần khi người dùng nhấp
