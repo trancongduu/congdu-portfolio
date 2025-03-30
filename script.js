3142ed2a1a212f8249a4dec3a2e1eb5319a8166b
trancongduu <126306009+trancongduu@users.noreply.github.com>
2025-03-31 02:29:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 console.log("File JavaScript đã được tải thành công!");
 
-  let audio = document.getElementById("myAudio");
-  audio.setAttribute("autoplay", "");
-  audio.muted = true; // Tắt tiếng để autoplay hoạt động
+  // let audio = document.getElementById("myAudio");
+  // audio.setAttribute("autoplay", "");
+  // audio.muted = true; // Tắt tiếng để autoplay hoạt động
 
-  document.getElementById("buttonStart").addEventListener("click", () => {
-    audio.play();
-    audio.muted = false; 
-    audio.volume = 0.1; 
-  }, { once: true }); // Chạy một lần khi người dùng nhấp vào nút
+  // document.getElementById("buttonStart").addEventListener("click", () => {
+  //   audio.play();
+  //   audio.muted = false; 
+  //   audio.volume = 0.1; 
+  // }, { once: true }); // Chạy một lần khi người dùng nhấp vào nút
 
 
 // Howler 
