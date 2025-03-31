2b56de3651f8846ac739b171f1ee520963718278
trancongduu <126306009+trancongduu@users.noreply.github.com>
2025-03-31 17:37:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ console.log("File JavaScript đã được tải thành công!");
   // }, { once: true }); // Chạy một lần khi người dùng nhấp vào nút
 
 
-// Howler 
+// Howler Audio
 $(document).ready(function() {
   let sound = new Howl({
     src: ["https://cdn.jsdelivr.net/gh/trancongduu/congdu-portfolio@main/Cyberpunk%20Edgerunners%20Let%20You%20Down.m4a"],
@@ -21,29 +21,24 @@ $(document).ready(function() {
       console.log('Finished!');
     }
   });
-
   let clickSound = new Howl({
     src: ["https://cdn.jsdelivr.net/gh/bedantpixeto/audio/ES_MM%20Beep%2043%20-%20SFX%20Producer.mp3"],
     volume: 0.3,
   });
-
   let hoverSound = new Howl({
     src: ["https://cdn.jsdelivr.net/gh/bedantpixeto/audio/hover-1.mp3"],
     volume: 0.2,
   });
-
   // Chỉ chạy nhạc khi user click vào nút #start
   $("#start").one("click", function (event) {
     sound.play();
   });
-
   // Không cho phép phát nhạc nếu bấm ra ngoài nút #start
   $(document).on("click", function (event) {
     if (!$(event.target).is("#start")) {
       console.log("Nhấn ngoài nút start, không phát nhạc.");
     }
   });
-
   // Toggle mute/unmute với localStorage
   if (localStorage.getItem("muteState") === "true") {
     Howler.mute(true);
@@ -52,23 +47,36 @@ $(document).ready(function() {
     Howler.mute(false);
     $("#sound").removeClass("muted");
   }
-
   $("#sound").on("click", function () {
     let isMuted = !Howler._muted;
     Howler.mute(isMuted);
     $(this).toggleClass("muted", isMuted);
     localStorage.setItem("muteState", isMuted.toString());
   });
-
   // Phát âm thanh khi click
   $("[data-click]").on("click", function () {
     clickSound.play();
   });
-
   // Phát âm thanh khi hover
   $("[data-hover]").on("mouseenter", function () {
     hoverSound.play();
   });
 });
+
+// Lenis scroll smooth
+const lenis = new Lenis({
+  duration: 1.2, // Điều chỉnh thời gian cuộn
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Easing mặc định
+  smooth: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+
+
 
 
