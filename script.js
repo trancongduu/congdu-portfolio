08435ee4642ae4c88d48aebaff61538ae3e74ee3
trancongduu <126306009+trancongduu@users.noreply.github.com>
2025-03-31 17:43:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -64,17 +64,52 @@ $(document).ready(function() {
 });
 
 // Lenis scroll smooth
-const lenis = new Lenis({
-  duration: 1.2, // Điều chỉnh thời gian cuộn
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Easing mặc định
-  smooth: true,
-});
+if (!document.querySelector("html").classList.contains('w-editor')){
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+    direction: 'vertical', // vertical, horizontal
+    gestureDirection: 'vertical', // vertical, horizontal, both
+    smooth: true,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+  })
 
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
+  // Get scroll value. This is just for testing purposes. Delete this if you're not using the scroll value for anything.
+  lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
+    console.log({ scroll, limit, velocity, direction, progress })
+  })
+
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+
+  // Grab all elements that have a "data-target" attribute
+  const scrollButtons = document.querySelectorAll('[data-target]');
+
+  // For each element, listen to a "click" event
+  scrollButtons.forEach(button => {
+    button.addEventListener('click', e => {
+      e.preventDefault();
+
+      // get the DOM element by the ID (data-target value)
+      var target = button.dataset.target,
+          $el = document.getElementById(target.replace('#', ''));
+
+      // Use lenis.scrollTo() to scroll the page to the right element
+      lenis.scrollTo($el, {
+        offset: 0, 
+        immediate: false,
+        duration: 3,
+        easing: (x) => (x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2), // https://easings.net
+      });
+    });
+  });
+
+  requestAnimationFrame(raf)
 }
-requestAnimationFrame(raf);
 
 
 
